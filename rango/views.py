2c4c477590752ead295edc43ddce8078d5bc8df1
Sasha <ivanchyshyn14@gmail.com>
2019-04-12 14:57:03 +0300

    Added to index template 'Top 5 categories and pages' and changed views to handle all added templates

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,32 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView, RedirectView, TemplateView
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Page
 
-class Index(ListView):
+class IndexView(ListView):
+    #queryset = Category.objects.order_by('-likes')[:5]
     model = Category
     template_name = 'rango/index.html'
+    context_object_name = 'categories'
+    extra_context = {'pages': Page.objects.order_by('-views')[:5]}
 
-def about(request):
-    context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/about.html', context)
+    def get_queryset(self):
+        return self.model._default_manager.order_by('-likes')[:5]
+
+class CategoryView(ListView):
+    model = Category
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        return self.model._default_manager.order_by('-likes')
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'rango/category_detail.html'
+
+class AboutRangoView(TemplateView):
+    extra_context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    template_name = 'rango/about.html'
+
+class IndexRedirectView(RedirectView):
+    url = 'rango/'
+    permanent = True
